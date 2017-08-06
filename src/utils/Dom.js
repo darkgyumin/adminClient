f686ebc446b48ed9efcdb1cb9e44c245081e1bbd
KimGyuMin <blackksj@nate.com>
2017-08-07 01:02:26 +0900

    adminLte

---
+++ src/utils/Dom.js
@@ -0,0 +1,29 @@
+(function(root, factory) {
+    if(typeof module !== 'undefined' && typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        var obj = (root.$min === undefined) 
+                    ? factory() : Object.assign(root.$min, factory());
+        root.$min = obj;
+    }
+}(this, function() {
+    var lib = {};
+
+    //Element 선택
+    lib.selector = function(selector) {
+        let elem = document.querySelectorAll(selector);
+        let result;
+
+        if(elem.length === 0) {
+            result = null;
+        } else if(elem.length === 1) {
+            result = elem[0];
+        } else {
+            result = elem;
+        }
+        
+        return result;
+    };
+
+    return lib;
+}));
---
+++ src/utils/Style.js
@@ -0,0 +1,35 @@
+(function(root, factory) {
+    if(typeof module !== 'undefined' && typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        var obj = (root.$min === undefined) 
+                    ? factory() : Object.assign(root.$min, factory());
+        root.$min = obj;
+    }
+}(this, function() {
+    var lib = {};
+
+    //기존 Class를 삭제하고 Class 추가
+    lib.initAddClass = function(elem, className) {
+        //기존 Class 삭제
+        let classList = Array.prototype.slice.apply(elem.classList);
+
+        classList.forEach((className) => {
+            elem.classList.remove(className);
+        });
+        //기존 Class 삭제
+
+        let arrClassName = className.split(' ');
+        
+        arrClassName.forEach((className) => {
+            this.addClass(elem, className);
+        });
+    };
+
+    //Class 적용
+    lib.addClass = function(elem, className) {
+        elem.classList.add(className);
+    };
+
+    return lib;
+}));
