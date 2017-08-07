c6ab3e4f19a252b12882693997236851ec8c6826
KimGyuMin <blackksj@naver.com>
2017-08-07 17:44:51 +0900

    library 추가

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,68 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Style from '../utils/Style';
 import Dom from '../utils/Dom';
 
-const Login = ({}) => {
+class Login extends Component {
 
-    Style.initAddClass(Dom.selector('body'), 'hold-transition register-page');
+    render() {
+        Style.initAddClass(Dom.selector('body'), 'hold-transition login-page');
 
-    return (
-        <div>
-            <div className="register-box">
-                <div className="register-logo">
-                    <a href="#admin"><b>Admin</b></a>
-                </div>
-
-                <div className="register-box-body">
-                    <p className="login-box-msg">Register a new membership</p>
-
-                    <form action="" method="post">
-                        <div className="form-group has-feedback">
-                            <input type="text" className="form-control" placeholder="Full name" />
-                            <span className="glyphicon glyphicon-user form-control-feedback"></span>
-                        </div>
-                        <div className="form-group has-feedback">
-                            <input type="email" className="form-control" placeholder="Email" />
-                            <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
-                        </div>
-                        <div className="form-group has-feedback">
-                            <input type="password" className="form-control" placeholder="Password" />
-                            <span className="glyphicon glyphicon-lock form-control-feedback"></span>
-                        </div>
-                        <div className="form-group has-feedback">
-                            <input type="password" className="form-control" placeholder="Retype password" />
-                            <span className="glyphicon glyphicon-log-in form-control-feedback"></span>
-                        </div>
-                        <div className="row">
-                            <div className="col-xs-8">
-                                <div className="checkbox icheck">
-                                    <label>
-                                    <input type="checkbox" /> I agree to the <a href="#">terms</a>
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="col-xs-4">
-                            <button type="submit" className="btn btn-primary btn-block btn-flat">Register</button>
-                            </div>
-                        </div>
-                    </form>
-
-                    <div className="social-auth-links text-center">
-                    <p>- OR -</p>
-                    <a href="#" className="btn btn-block btn-social btn-facebook btn-flat"><i className="fa fa-facebook"></i> Sign up using
-                        Facebook</a>
-                    <a href="#" className="btn btn-block btn-social btn-google btn-flat"><i className="fa fa-google-plus"></i> Sign up using
-                        Google+</a>
+        return (
+            <div>
+                <div className="register-box">
+                    <div className="register-logo">
+                        <a href="#admin"><b>Admin</b></a>
                     </div>
 
-                    <a href="login.html" className="text-center">I already have a membership</a>
+                    <div className="register-box-body">
+                        <p className="login-box-msg">Sign in to start your session</p>
+
+                        <form action="" method="post">
+                            <div className="form-group has-feedback">
+                                <input type="email" className="form-control" placeholder="Email" />
+                                <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
+                            </div>
+                            <div className="form-group has-feedback">
+                                <input type="password" className="form-control" placeholder="Password" />
+                                <span className="glyphicon glyphicon-lock form-control-feedback"></span>
+                            </div>
+                            <div className="row">
+                                <div className="col-xs-8">
+                                    <div className="checkbox icheck">
+                                        <label>
+                                        <input type="checkbox" /> Remember Me
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="col-xs-4">
+                                <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <div className="social-auth-links text-center">
+                        <p>- OR -</p>
+                        <a href="#facebook" className="btn btn-block btn-social btn-facebook btn-flat"><i className="fa fa-facebook"></i> Sign in using Facebook</a>
+                        <a href="#google" className="btn btn-block btn-social btn-google btn-flat"><i className="fa fa-google-plus"></i> Sign in using Google+</a>
+                        </div>
+
+                        <a href="login.html" className="text-center">I already have a membership</a>
+                        
+                    </div>
                     
                 </div>
-                
             </div>
-        </div>
-    );
+        );
+    }
+
+    componentDidMount() {
+        let $ = window.$;
+        
+        $(() => {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+        });
+    }
 };
 
 Login.propTypes = {
