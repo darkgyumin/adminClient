57443b13d0cb00024feffb491a19ec8c148e5f4c
KimGyuMin <blackksj@nate.com>
2017-08-09 00:49:50 +0900

    layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import Login from './routes/Login';
 import Register from './routes/Register';
+import Layout from './routes/Layout';
 
 import './App.css';
+import './all-skins.min.css';
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
           <Route exact path="/" component={Login} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} /> 
+          <Route path="/layout" component={Layout} /> 
         </Switch>
       </Router>
     );
--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Style from '../utils/Style';
+import Dom from '../utils/Dom';
 
 class Layout extends Component {
+
     render() {
+        Style.initAddClass(Dom.selector('body'), 'hold-transition skin-blue sidebar-mini');
+
         return (
-            <div>
-                
+            <div className="wrapper">
+
+                <header className="main-header">
+                    <a href="../../index2.html" className="logo">
+                        <span className="logo-mini"><b>A</b>LT</span>
+                        <span className="logo-lg"><b>Admin</b>LTE</span>
+                    </a>
+                    <nav className="navbar navbar-static-top">
+                        <a href="#" className="sidebar-toggle" data-toggle="offcanvas" role="button">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </a>
+
+                        <div className="navbar-custom-menu">
+                            
+                        </div>
+                    </nav>
+                </header>
+
+                <aside className="main-sidebar">
+                    <section className="sidebar">
+                        <div className="user-panel">
+                            <div className="pull-left image">
+                                <img src="../img/user2-160x160.jpg" className="img-circle" alt="User Image" />
+                            </div>
+                            <div className="pull-left info">
+                                <p>Alexander Pierce</p>
+                                <a href="#"><i className="fa fa-circle text-success"></i> Online</a>
+                            </div>
+                        </div>   
+                    </section>
+                </aside>
             </div>
         );
     }
